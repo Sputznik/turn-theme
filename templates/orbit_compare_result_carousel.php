2860b6614c1d0048105a1a6fd34adbc90b78f298
unknown <>
2021-02-23 12:37:02 +0530

    Change left & right to div buttons

--- templates/orbit_compare_result_carousel.php
+++ templates/orbit_compare_result_carousel.php
@@ -59,14 +59,14 @@
 	  </ol>
 
 	  <!-- Controls -->
-	  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+	  <div class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
 	    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 	    <span class="sr-only">Previous</span>
-	  </a>
-	  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+	  </div>
+	  <div class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
 	    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 	    <span class="sr-only">Next</span>
-	  </a>
+	  </div>
 	</div>
 
 </div>
